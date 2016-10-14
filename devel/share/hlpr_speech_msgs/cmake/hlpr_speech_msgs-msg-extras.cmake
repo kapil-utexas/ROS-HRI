bff38fd170e18a19fa52ddc03249d2bcbbdb3d6c
kapil <kapil@utexas.edu>
2016-10-14 08:30:17 -0500

    catkin_make for speech recognition working

---
+++ devel/share/hlpr_speech_msgs/cmake/hlpr_speech_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(hlpr_speech_msgs_MESSAGE_FILES "/home/caee-ks36732/catkin_ws/src/hlpr_speech/hlpr_speech_msgs/msg/StampedString.msg;/home/caee-ks36732/catkin_ws/src/hlpr_speech/hlpr_speech_msgs/msg/SpeechCommand.msg")
+set(hlpr_speech_msgs_SERVICE_FILES "/home/caee-ks36732/catkin_ws/src/hlpr_speech/hlpr_speech_msgs/srv/SpeechService.srv")
---
+++ devel/share/hlpr_speech_msgs/cmake/hlpr_speech_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(hlpr_speech_msgs_MSG_INCLUDE_DIRS "/home/caee-ks36732/catkin_ws/src/hlpr_speech/hlpr_speech_msgs/msg")
+set(hlpr_speech_msgs_MSG_DEPENDENCIES std_msgs)
